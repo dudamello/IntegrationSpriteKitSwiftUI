f2d8c64c4936bdb688f367bb45aed971a8b8ff4d
Eduarda Mello <duda.mello1@hotmail.com>
2020-09-16 10:12:55 -0300

    Initial Commit

---
+++ integration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  integration
+//
+//  Created by Eduarda Mello on 16/09/20.
+//  Copyright © 2020 Eduarda Mello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
